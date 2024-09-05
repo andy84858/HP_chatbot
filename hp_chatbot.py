6fccf8f298cb58b28f6b95570f929354deeaf20a
Andy Lin <andy84858@gmail.com>
2024-09-05 18:48:51 +0800

    訊息

--- hp_chatbot.py
+++ hp_chatbot.py
@@ -96,7 +96,7 @@ def generate_response(query, language, chat_history):
     if language == 'zh':
         prompt_template = ChatPromptTemplate.from_template(
             "你是一個專門回答關於哈利波特問題的AI助手。請考慮以下對話歷史，並以台灣翻譯的繁體中文版小說的內容回答最新的問題：\n\n"
-            "如果上下文中並未提到具體訊息，請回答不知道"
+            "如果上下文中並未提到具體訊息，請依你對哈利波特小說繁體中文版的理解進行回答"
             "對話歷史：\n{history}\n\n"
             "上下文：\n{context}\n\n"
             "Human: {query}\n"
@@ -106,7 +106,7 @@ def generate_response(query, language, chat_history):
         prompt_template = ChatPromptTemplate.from_template(
             "You are an AI assistant specializing in answering questions about Harry Potter. "
             "Please consider the following conversation history and answer the latest question in English:\n\n"
-            "If context contains no specific content, please answer you don't know"
+            "If context contains no specific content, please answer the question based on your understanding to the Harry Potter novels"
             "Conversation history:\n{history}\n\n"
             "Context:\n{context}\n\n"
             "Human: {query}\n"
