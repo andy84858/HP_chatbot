5ee2a445362b54ace1d84241ad718f225a0ea5fb
Andy Lin <andy84858@gmail.com>
2024-09-05 16:01:00 +0800

    Update hp_chatbot.py
    Add connection to AWS S3 bucket, so that Streamlit can access vector database.

--- hp_chatbot.py
+++ hp_chatbot.py
@@ -1,11 +1,55 @@
 import streamlit as st
+import boto3
 import os
+import tarfile
 import langid
 from langchain.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
+from langchain_community.vectorstores import Chroma
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 # 設置 OpenAI API 密鑰
-os.environ['OPENAI_API_KEY'] = 'OPENAI_API_KEY'
+
+os.environ['OPENAI_API_KEY'] = st.secrets["OPENAI_API_KEY"]
+
+# Setting AWS Credential
+s3 = boto3.client('s3',
+                  aws_access_key_id=st.secrets["AWS_ACCESS_KEY_ID"],
+                  aws_secret_access_key=st.secrets["AWS_SECRET_ACCESS_KEY"])
+
+@st.cache_resource
+def load_db_from_s3(lang):
+    bucket_name = st.secrets["S3_BUCKET_NAME"]
+    file_name = f'hp_{lang}_database.tar.gz'
+    local_file = f'/tmp/hp_{lang}_database.tar.gz'
+    local_dir = f'/tmp/hp_{lang}_database'
+
+    try:
+        # Download files from S3 bucket
+        s3.download_file(bucket_name, file_name, local_file)
+
+        # Unzip files
+        with tarfile.open(local_file, 'r:gz') as tar:
+            tar.extractall(path='/tmp')
+
+        # Laod Chroma dataset
+        embedding_function = OpenAIEmbeddings()
+        db = Chroma(persist_directory=local_dir, embedding_function=embedding_function)
+
+        st.success(f"Successfully loaded {lang.upper()} database")
+        return db
+    except Exception as e:
+        st.error(f"Error loading {lang.upper()} database: {str(e)}")
+        return None
+
+# 加载中英文数据库Load CN/EN database
+@st.cache_resource
+def load_databases():
+    cn_db = load_db_from_s3('cn')
+    en_db = load_db_from_s3('en')
+    return cn_db, en_db
+
+cn_db, en_db = load_databases()
+
 
 # 創建聊天模型
 chat_model = ChatOpenAI(model_name="gpt-4o-mini")
@@ -15,13 +59,22 @@ def detect_language(text):
     return 'zh' if lang == 'zh' else 'en'
 
 def generate_response(query, language, chat_history):
-    # 將聊天歷史轉換為字符串格式
+    db = cn_db if language == 'zh' else en_db
+    if db is None:
+        return "I'm sorry, but the required database is not available at the moment. Please try again later."
+
+    results = db.similarity_search_with_relevance_scores(query, k=5)
+    context = "\n".join([doc.page_content for doc, _ in results])
+    # 將聊天歷史轉換為str格式
     history_str = "\n".join([f"Human: {msg['content']}" if msg['role'] == 'user' else f"AI: {msg['content']}" for msg in chat_history])
 
     if language == 'zh':
         prompt_template = ChatPromptTemplate.from_template(
             "你是一個專門回答關於哈利波特問題的AI助手。請考慮以下對話歷史，並以繁體中文版小說的翻譯名稱回答最新的問題：\n\n"
+            "如果上下文中並未提到具體訊息，請以自身的認知進行回答"
+            "如果超出認知，請具體回答不知道"
             "對話歷史：\n{history}\n\n"
+            "上下文：\n{context}\n\n"
             "Human: {query}\n"
             "AI: "
         )
@@ -29,12 +82,15 @@ def generate_response(query, language, chat_history):
         prompt_template = ChatPromptTemplate.from_template(
             "You are an AI assistant specializing in answering questions about Harry Potter. "
             "Please consider the following conversation history and answer the latest question in English:\n\n"
+            "If context contains no specific content, please answer question based on your own understanding"
+            "If question beyond your understanding, please say you don't know"
             "Conversation history:\n{history}\n\n"
+            "Context:\n{context}\n\n"
             "Human: {query}\n"
             "AI: "
         )
 
-    prompt = prompt_template.format(history=history_str, query=query)
+    prompt = prompt_template.format(history=history_str, context=context, query=query)
     response = chat_model.invoke(prompt)
 
     return response.content
